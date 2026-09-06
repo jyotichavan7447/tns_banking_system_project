370f0e5bd122dc176fe1c6868ad4777841be2e06
Pratiksha-IT <pratikshasatpute2005@gmail.com>
2026-09-06 19:22:57 +0530

    Add files via upload

---
+++ com/tns/banking/model/Transaction.java
@@ -0,0 +1,33 @@
+package com.tns.banking.model;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private int transactionID;
+    private int accountID;
+    private String type;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Transaction(int accountID, String type, double amount) {
+        this.accountID = accountID;
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public int getTransactionID() { return transactionID; }
+    public void setTransactionID(int transactionID) { this.transactionID = transactionID; }
+    public int getAccountID() { return accountID; }
+    public void setAccountID(int accountID) { this.accountID = accountID; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
+    public String toString() {
+        return "Transaction ID: " + transactionID + ", Type: " + type + ", Amount: " + amount + ", Timestamp: " + timestamp;
+    }
+}
